0ae1560d17a71c49d6465583f3c5371d236fe8a1
Alexander Mire <amire51117@gmail.com>
2018-08-23 22:50:42 -0400

    add mutation and external schema

--- src/index.js
+++ src/index.js
@@ -1,40 +1,34 @@
 const { GraphQLServer } = require('graphql-yoga');
 
-const typeDefs = `
-type Query {
-	info: String!
-	feed: [Link!]!
-}
-
-type Link {
-	id: ID!
-	description: String!
-	url: String!
-}
-`
 let links = [{
-	id: 'link-0',
-	url: 'www.howtographql.com',
-	description: 'Fullstack tutorial for GraphQL'
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
 }]
-
+// 1
+let idCount = links.length
 const resolvers = {
-	Query: {
-		info: () => `This is the API of a Hackernews Clone`,
-		feed: () => links,	
-	},
-
-	Link: {
-		id: (root) => root.id,
-		description: (root) => root.description,
-		url: (root) => root.url,
-	}
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
+  Mutation: {
+    // 2
+    post: (root, args) => {
+       const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      }
+      links.push(link)
+      return link
+    }
+  },
 }
 
-const server = new GraphQLServer(
-{
-	typeDefs,
-	resolvers,
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers,
 })
 
 server.start(() => console.log(`Server is running on port 4000`))
