8b3d1d69af979018f92ccab1e25a5c68bd35b4bb
Alexander Mire <amire51117@gmail.com>
2018-08-25 22:19:36 -0400

    add filtering and pagination

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ const { Prisma } = require('prisma-binding');
 const Query = require('./resolvers/Query');
 const Mutation = require('./resolvers/Mutation');
 const AuthPayload = require('./resolvers/AuthPayload');
+const Subscription = require('./resolvers/Subscription');
+const Feed = require('./resolvers/Feed');
+
 // 1
 const resolvers = {
   Query,
   Mutation,
-  AuthPayload
+  AuthPayload,
+  Subscription,
+  Feed,
 }
 
 const server = new GraphQLServer({
--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -2,6 +2,21 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { APP_SECRET, getUserId } = require('../utils');
 
+
+function post(parent, args, context, info) {
+  const userId = getUserId(context)
+  return context.db.mutation.createLink(
+    {
+      data: {
+        url: args.url,
+        description: args.description,
+        postedBy: { connect: { id: userId } },
+      },
+    },
+    info,
+  )
+}
+
 async function signup(parent, args, context, info) {
 	const password = await bcrypt.hash(args.password, 10);
 
@@ -34,22 +49,31 @@ async function login(parent, args, context, info) {
 	}
 }
 
-function post(parent, args, context, info) {
-  const userId = getUserId(context);
-  return context.db.mutation.createLink(
-    {
-      data: {
-        url: args.url,
-        description: args.description,
-        postedBy: { connect: { id: userId } },
-      },
-    },
-    info,
-  )
+async function vote(parent, args, context, info) {
+	const userId = getUserId(context);
+
+	const linkExists = await context.db.exists.Vote({
+		user: { id: userId },
+		link: { id: args.linkId },
+	})
+	if (linkExists) {
+		throw new Error(`Already voted for link: ${args.linkId}`)
+	}
+
+	return context.db.mutation.createVote(
+		{
+			data: {
+				user: { connect: { id: userId }},
+				link: { connect: { id: args.linkId }},
+			},
+		},
+		info,
+	)
 }
 
 module.exports = {
 	signup,
 	login,
 	post,
+	vote,
 }
